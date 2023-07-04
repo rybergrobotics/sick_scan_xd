c43242adc02fc74d7987461b55f047d9bca8ec13
rostest <ros.test@lehning.de>
2023-07-04 16:05:09 +0200

    Preparation for bloom release

---
+++ test/scripts/run_linux_ros1_bloom.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+printf "\033c"
+rm -rf /tmp/prerelease_job
+mkdir -p /tmp/prerelease_job
+pushd /tmp/prerelease_job
+
+generate_prerelease_script.py \
+          https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/production/index.yaml \
+          noetic default ubuntu focal amd64 \
+          --custom-repo \
+            sick_scan_xd:git:https://github.com/SICKAG/sick_scan_xd:feature/bloom_pretest \
+            msgpack11:git:https://github.com/SICKAG/msgpack11:master \
+            libsick_ldmrs:git:https://github.com/SICKAG/libsick_ldmrs:master \
+          --level 1 \
+          --output-dir ./        
+
+rm -rf ~/.ccache ; mkdir -p ~/.ccache ; ./prerelease.sh
+popd
+
